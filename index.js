19fb00e6dff072b992891404e395dc8b4e81905e
rehan700 <rehansource3@gmail.com>
2024-09-10 11:55:35 +0530

    added env variable

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
+
 const mongoConnection = require('./db')
 mongoConnection()
-
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000
 app.use(express.json())
 // Your routes and other logic here
 app.get('/', (req, res) => {
@@ -12,6 +13,6 @@ app.get('/', (req, res) => {
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 app.use('/api/test', require('./routes/test'))
-app.listen(port, () => {
-    console.log(`App running on http://localhost:${port}`);
+app.listen(process.env.PORT, () => {
+    console.log(`App running on http://localhost:${port}`)
 });
